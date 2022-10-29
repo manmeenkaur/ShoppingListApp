8d10f2b903c90a8e71192fef05765d557816fef8
manmeenkaur <manmeen.kaur.17@gmail.com>
2022-10-29 14:02:40 -0400

    first commit

---
+++ ShoppingListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingListApp
+//
+//  Created by Manmeen Kaur on 2022-10-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
