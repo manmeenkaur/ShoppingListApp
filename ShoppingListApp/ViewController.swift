f49177887f9708050d37c69d5378d589d37c3588
manmeenkaur <manmeen.kaur.17@gmail.com>
2022-10-30 22:16:19 -0400

    added textfield and label functionality in both portrait and landscape views

--- ShoppingListApp/ViewController.swift
+++ ShoppingListApp/ViewController.swift
@@ -13,9 +13,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // App Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func reset()
+    {
+        ShoppingListName.text = "Shopping List"
+        TextField1.text = ""
+        TextField2.text = ""
+        TextField3.text = ""
+        TextField4.text = ""
+        TextField5.text = ""
+        Label1.text = "0"
+        Label2.text = "0"
+        Label3.text = "0"
+        Label4.text = "0"
+        Label5.text = "0"
+        ShoppingListNameLandscape.text = "Shopping List"
+        TextFieldLandscape1.text = ""
+        TextFieldLandscape2.text = ""
+        TextFieldLandscape3.text = ""
+        TextFieldLandscape4.text = ""
+        TextFieldLandscape5.text = ""
+        LabelLandscape1.text = "0"
+        LabelLandscape2.text = "0"
+        LabelLandscape3.text = "0"
+        LabelLandscape4.text = "0"
+        LabelLandscape5.text = "0"
     }
     
     //Textfields to enter shopping list name
@@ -79,46 +106,66 @@ class ViewController: UIViewController {
     //Steppers to increase and decrease the quantity of items
     @IBAction func Stepper1(_ sender: UIStepper) {
         Label1.text=String(Int(sender.value))
+        LabelLandscape1.text = Label1.text
+        TextFieldLandscape1.text = TextField1.text
     }
     
     @IBAction func StepperLandscape1(_ sender: UIStepper) {
         LabelLandscape1.text=String(Int(sender.value))
+        Label1.text = LabelLandscape1.text
+        TextField1.text = TextFieldLandscape1.text
     }
     
     
     @IBAction func Stepper2(_ sender: UIStepper) {
         Label2.text=String(Int(sender.value))
+        LabelLandscape2.text = Label2.text
+        TextFieldLandscape2.text = TextField2.text
     }
     
     @IBAction func StepperLandscape2(_ sender: UIStepper) {
         LabelLandscape2.text=String(Int(sender.value))
+        Label2.text = LabelLandscape2.text
+        TextField2.text = TextFieldLandscape2.text
     }
     
     
     @IBAction func Stepper3(_ sender: UIStepper) {
         Label3.text=String(Int(sender.value))
+        LabelLandscape3.text = Label3.text
+        TextFieldLandscape3.text = TextField3.text
     }
     
     @IBAction func StepperLandscape3(_ sender: UIStepper) {
         LabelLandscape3.text=String(Int(sender.value))
+        Label3.text = LabelLandscape3.text
+        TextField3.text = TextFieldLandscape3.text
     }
     
     
     @IBAction func Stepper4(_ sender: UIStepper) {
         Label4.text=String(Int(sender.value))
+        LabelLandscape4.text = Label4.text
+        TextFieldLandscape4.text = TextField4.text
     }
     
     @IBAction func StepperLandscape4(_ sender: UIStepper) {
         LabelLandscape4.text=String(Int(sender.value))
+        Label4.text = LabelLandscape4.text
+        TextField4.text = TextFieldLandscape4.text
     }
     
     
     @IBAction func Stepper5(_ sender: UIStepper) {
         Label5.text=String(Int(sender.value))
+        LabelLandscape5.text = Label5.text
+        TextFieldLandscape5.text = TextField5.text
     }
     
     @IBAction func StepperLandscape5(_ sender: UIStepper) {
         LabelLandscape5.text=String(Int(sender.value))
+        Label5.text = LabelLandscape5.text
+        TextField5.text = TextFieldLandscape5.text
     }
     
     
@@ -127,15 +174,36 @@ class ViewController: UIViewController {
         if(ShoppingListName.text == "")
         {
             ShoppingListName.text = "Grocery Shopping List"
+            ShoppingListNameLandscape.text = ShoppingListName.text
         }
         print("***************************")
         print(ShoppingListName.text! as Any)
         print("***************************")
-        print("\(String(describing: TextField1.text!))","--> Qty:","\(String(describing: Label1.text!))")
-        print("\(String(describing: TextField2.text!))","--> Qty:","\(String(describing: Label2.text!))")
-        print("\(String(describing: TextField3.text!))","--> Qty:","\(String(describing: Label3.text!))")
-        print("\(String(describing: TextField4.text!))","--> Qty:","\(String(describing: Label4.text!))")
-        print("\(String(describing: TextField5.text!))","--> Qty:","\(String(describing: Label5.text!))")
+        if(TextField1.text != "" && Label1.text != "0")
+        {
+            print("\(String(describing: TextField1.text!))","--> Qty:","\(String(describing: Label1.text!))")
+            TextFieldLandscape1.text = TextField1.text
+        }
+        if(TextField2.text != "" && Label2.text != "0")
+        {
+            print("\(String(describing: TextField2.text!))","--> Qty:","\(String(describing: Label2.text!))")
+            TextFieldLandscape2.text = TextField2.text
+        }
+        if(TextField3.text != "" && Label3.text != "0")
+        {
+            print("\(String(describing: TextField3.text!))","--> Qty:","\(String(describing: Label3.text!))")
+            TextFieldLandscape3.text = TextField3.text
+        }
+        if(TextField4.text != "" && Label4.text != "0")
+        {
+            print("\(String(describing: TextField4.text!))","--> Qty:","\(String(describing: Label4.text!))")
+            TextFieldLandscape4.text = TextField4.text
+        }
+        if(TextField5.text != "" && Label5.text != "0")
+        {
+            print("\(String(describing: TextField5.text!))","--> Qty:","\(String(describing: Label5.text!))")
+            TextFieldLandscape5.text = TextField5.text
+        }
         print("***************************")
     }
     
@@ -143,45 +211,46 @@ class ViewController: UIViewController {
         if(ShoppingListNameLandscape.text == "")
         {
             ShoppingListNameLandscape.text = "Grocery Shopping List"
+            ShoppingListName.text = ShoppingListNameLandscape.text
         }
         print("***************************")
         print(ShoppingListNameLandscape.text! as Any)
         print("***************************")
-        print("\(String(describing: TextFieldLandscape1.text!))","--> Qty:","\(String(describing: LabelLandscape1.text!))")
-        print("\(String(describing: TextFieldLandscape2.text!))","--> Qty:","\(String(describing: LabelLandscape2.text!))")
-        print("\(String(describing: TextFieldLandscape3.text!))","--> Qty:","\(String(describing: LabelLandscape3.text!))")
-        print("\(String(describing: TextFieldLandscape4.text!))","--> Qty:","\(String(describing: LabelLandscape4.text!))")
-        print("\(String(describing: TextFieldLandscape5.text!))","--> Qty:","\(String(describing: LabelLandscape5.text!))")
+        if(TextFieldLandscape1.text != "" && LabelLandscape1.text != "0")
+        {
+            print("\(String(describing: TextFieldLandscape1.text!))","--> Qty:","\(String(describing: LabelLandscape1.text!))")
+            TextField1.text = TextFieldLandscape1.text
+        }
+        if(TextFieldLandscape2.text != "" && LabelLandscape2.text != "0")
+        {
+            print("\(String(describing: TextFieldLandscape2.text!))","--> Qty:","\(String(describing: LabelLandscape2.text!))")
+            TextField2.text = TextFieldLandscape2.text
+        }
+        if(TextFieldLandscape3.text != "" && LabelLandscape3.text != "0")
+        {
+            print("\(String(describing: TextFieldLandscape3.text!))","--> Qty:","\(String(describing: LabelLandscape3.text!))")
+            TextField3.text = TextFieldLandscape3.text
+        }
+        if(TextFieldLandscape4.text != "" && LabelLandscape4.text != "0")
+        {
+            print("\(String(describing: TextFieldLandscape4.text!))","--> Qty:","\(String(describing: LabelLandscape4.text!))")
+            TextField4.text = TextFieldLandscape4.text
+        }
+        if(TextFieldLandscape5.text != "" && LabelLandscape5.text != "0")
+        {
+            print("\(String(describing: TextFieldLandscape5.text!))","--> Qty:","\(String(describing: LabelLandscape5.text!))")
+            TextField5.text = TextFieldLandscape5.text
+        }
         print("***************************")
     }
     
     
     //Reseting shopping list app when clicking Cancel button
     @IBAction func CancelList(_ sender: UIButton) {
-        ShoppingListName.text = "Shopping List"
-        TextField1.text = ""
-        TextField2.text = ""
-        TextField3.text = ""
-        TextField4.text = ""
-        TextField5.text = ""
-        Label1.text = "0"
-        Label2.text = "0"
-        Label3.text = "0"
-        Label4.text = "0"
-        Label5.text = "0"
+        reset()
     }
     
     @IBAction func CancelListLandscape(_ sender: UIButton) {
-        ShoppingListNameLandscape.text = "Shopping List"
-        TextFieldLandscape1.text = ""
-        TextFieldLandscape2.text = ""
-        TextFieldLandscape3.text = ""
-        TextFieldLandscape4.text = ""
-        TextFieldLandscape5.text = ""
-        LabelLandscape1.text = "0"
-        LabelLandscape2.text = "0"
-        LabelLandscape3.text = "0"
-        LabelLandscape4.text = "0"
-        LabelLandscape5.text = "0"
+        reset()
     }
 }
